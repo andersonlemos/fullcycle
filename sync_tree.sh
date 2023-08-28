9ec571f29fbbc4730a6582d0056b671daa201261
Anderson Lemos <alemos@protonmail.com>
2023-08-28 10:33:11 -0300

    feat(order): create tests and implementation repository

--- sync_tree.sh
+++ sync_tree.sh
@@ -3,3 +3,5 @@
 git subtree "$1" --prefix=ddd/fullcycle-desafio-ddd-patterns fullcycle-desafio-ddd-patterns main
 git subtree "$1" --prefix=docker/fullcycle-desafio-go fullcycle-desafio-go main
 git subtree "$1" --prefix=docker/fullcycle-nginx-with-nodejs fullcycle-nginx-with-nodejs main
+
+git push origin main
